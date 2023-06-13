f0226c7a5c524b4365ed36b2399c3f42266f5ee3
JustSkyDev <68405171+JustSkyDev@users.noreply.github.com>
2023-06-13 00:46:54 +0000

    modified:   scripts/JS/plugins/Custom Commands/example/types.js

--- scripts/JS/plugins/Custom Commands/example/types.js
+++ scripts/JS/plugins/Custom Commands/example/types.js
@@ -8,4 +8,10 @@ new CommandRegistration()
   .setPrivate(true) // Set command to Operator only (optional)
   .setRequireTags(["example"]) // If player had "example" tag, player can run the command (optional)
   .setExample(["example"]) // Set command example use (optional)
-  .setUsage(["example"]); // Set command usage (optional)
+  .setUsage(["example"]) // Set command usage (optional)
+  .setInputs({
+    0: ["string"],
+    1: ["boolean"],
+    2: ["number"],
+    3: ["playername"],
+  }); // Set command input (args) (optional)
